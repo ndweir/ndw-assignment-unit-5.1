dd5fa1f0b71e742f51dda2f4d1c76934f3a5e6f1
nick <ndweir13@gmail.com>
2024-08-28 11:33:15 -0500

    accessedObjProperties

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -19,20 +19,23 @@ const me = {
 };
 console.log('A little about me:', me);
 
+
 /* 2. Accessing object properties.
   - Create a variable called fullName 
   - Use the firstName & lastName properties of the object you 
     created above to set its value. 
   - Console.log fullName
 */
-
+let fullName = me.firstName + ' ' + me.lastName;
+console.log(fullName);
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
+console.log(me.favThreeFoods[0])
+console.log(me.favThreeFoods[2])
 
 
 /* 4. Change a property of an existing object.
@@ -41,6 +44,9 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log(me.shoeCount);
+me.shoeCount++;
+console.log(me.shoeCount);
 
 
 /* 5. Add a new property to an existing object.
